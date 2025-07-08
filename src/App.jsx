558ab73936b6c685171ace0f33c2ed46858c6a26
bAtmAn2908 <prajesh.patel293@gmail.com>
2025-07-08 12:32:48 -0400

    Hero section desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './pages/Navbar'
+import Hero from './pages/Hero'
 
 const App = () => {
   return (
     <div className='container mx-auto max-w-7xl'>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/components/HeroText.jsx
@@ -0,0 +1,26 @@
+import { FlipWords } from "./Flipwords"
+
+const HeroText = () => {
+  return (
+    <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
+        {/* Desktopview */}
+        <div className="flex-col hidden md:flex c-space">
+            <h1 className="text-4xl font-medium">Hi I'm Prajesh</h1>
+            <div className="flex flex-col items-start">
+                <p className="text-5xl font-medium text-neutral-300">
+                    Full-Stack Explorer<br/> with a Frontend Heart
+                </p>
+                <div>
+                       <FlipWords words={["React.js","Next.js","Node.js","AWS","SQL"]} className='font-black text-white text-5xl'/>
+                </div>
+            </div>
+        </div>
+        {/* MobileView */}
+        <div className=""> 
+
+        </div>
+    </div>
+  )
+}
+
+export default HeroText
