4565d6abf75f88a296910632596f405450a389cf
bAtmAn2908 <prajesh.patel293@gmail.com>
2025-07-14 10:15:31 -0400

    Astronaut completed

--- src/components/Astronaut.jsx
+++ src/components/Astronaut.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
+import { useMotionValue, useSpring } from 'motion/react'
+import { useFrame } from '@react-three/fiber'
 
 export function Astronaut(props) {
   const group = useRef()
@@ -10,6 +12,17 @@ export function Astronaut(props) {
     actions[animations[0].name]?.play();
    }
   }, [actions, animations])
+
+  const yPosition = useMotionValue(5);
+  const ySpring = useSpring(yPosition, {damping: 30})
+  useEffect(() => {
+      ySpring.set(-1);
+  }, [ySpring])
+
+  useFrame(() => {
+    group.current.position.y = ySpring.get();
+  })
+  
   
   return (
     <group ref={group} {...props} dispose={null}  rotation={[-Math.PI / 2, -0.2, 2.2]} scale={props.scale || 0.3} position={props.position || [1.3, -1, 0]}>
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,10 +1,13 @@
-import {Canvas} from "@react-three/fiber";
+import {Canvas, useFrame} from "@react-three/fiber";
 import HeroText from "../components/HeroText"
 import ParallaxBackground from "../components/ParallaxBackground"
 import {Astronaut} from "../components/Astronaut";
-import { OrbitControls } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
+import { easing } from "maath";
+import { Float } from "@react-three/drei";
 
 const Hero = () => {
+  const isMobile = useMediaQuery({ maxWidth:853})
   return (
     <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
       <HeroText/>
@@ -12,12 +15,23 @@ const Hero = () => {
       <figure className="absolute inset-0"
           style={{width: "100vw", height: "100vh"}}>
             <Canvas camera={{ position: [0,1,3]}}>
-              <Astronaut scale={0.23} position={[0,-1.5,0]}/>
-              <OrbitControls/>
+              <Float>
+                <Astronaut scale={isMobile && 0.2} position={isMobile && [0,-1.5,0]}/>
+              </Float>        
+              <Rig/>
             </Canvas>
       </figure>
     </section>
   )
+}
+
+function Rig(){
+  return useFrame((state, delta) => {
+    easing.damp3(
+        state.camera.position,
+        [state.mouse.x / 10,1 + state.mouse.y / 10,3], 0.5, delta
+    )
+  })
 }
 
 export default Hero
